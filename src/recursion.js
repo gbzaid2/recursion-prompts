947f97314c1e06fcc830b6f282a55b93b164f4a9
Zaid <gbzaid2@gmail.com>
2019-12-24 10:04:54 -0500

    Done

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,86 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+
+    if (n === 0 || n === 1) return 1;
+    if (n < 0) return null;
+    return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    // base case would be length of sum being 0,
+    // recursive step
+    // array[0] + sum(array.slice(1))
+    // sum 
+    // [1].slice(1) -> ?
+    // [].slice(1) -> ?
+    // [1,2].slice(1) -> 2
+    if (array.length === 0) return 0;
+    return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++){
+        if (Array.isArray(array[i])){
+            sum += arraySum(array[i]);
+        } else {
+            sum += array[i];
+        }
+    }
+    return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+    if (n === 0) {
+        return true;
+    } else if (n === 1 || n === -1){
+        return false;
+    }
+
+    if (n < 0){
+        return isEven(n + 2);
+    } else {
+        return isEven(n - 2);
+    }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+
+    if ( n === 1 || n === 0) return 0;
+    return 0 + sumBelow(n-1);
+    
+    
+
 };
+
+var sumBelowHelper = function(n){
+    
+    if (n === 1){
+        return 1;
+    } else {
+        return n + sumBelowHelper(n - 1);
+    }
+}
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+    var results = [];
+    if (x === y) {
+        return;
+    } else {
+        results.push(x)
+    }
+    results.push();
 };
 
 // 7. Compute the exponent of a number.
